42ff7c2e86c402efb9db5b1adf92ff74800c3e39
Matthew Peveler <matt.peveler@gmail.com>
2020-04-16 11:54:48 -0400

    print out test coverage when running test
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!tests/'],
+  coverageReporters: ["text"]
 };
